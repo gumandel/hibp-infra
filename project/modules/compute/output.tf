f0cfe405a1d4afcea4440823f20ad935af8509dc
Gustavo Mandel <gustavomandel123@hotmail.com>
2025-05-02 11:04:33 -0300

    add: add output instance ids

--- project/modules/compute/output.tf
+++ project/modules/compute/output.tf
@@ -12,3 +12,13 @@ output "backend_private_ips" {
   description = "Lista de IPs privados das instâncias backend"
   value       = aws_instance.backend[*].private_ip
 }
+
+output "frontend_instance_ids" {
+  description = "Lista de IDs das instâncias EC2s frontend"
+  value       =  aws_instance.frontend[*].id
+}
+
+output "backend_instance_ids" {
+  description = "Lista de IDs das instâncias EC2s backend"
+  value       =  aws_instance.backend[*].id
+}
--- project/outputs.tf
+++ project/outputs.tf
@@ -1,9 +1,19 @@
 output "frontend_public_ips" {
+  description = "IPs públicos das instâncias frontend"
   value = module.frontend_backend.frontend_public_ips
-  description = "Public IPs of the frontend instances"
 }
 
 output "backend_public_ips" {
+  description = "IPs públicos das instâncias backend"
   value = module.frontend_backend.backend_public_ips
-  description = "Public IPs of the backend instances"
+}
+
+output "frontend_instance_ids" {
+  description = "Lista de IDs das instâncias EC2s frontend"
+  value       =  module.frontend_backend.frontend_instance_ids
+}
+
+output "backend_instance_ids" {
+  description = "Lista de IDs das instâncias EC2s backend"
+  value       = module.frontend_backend.backend_instance_ids
 }
